5e37dfd0ff87d07ea74fa882ca8243c1c3c78cb7
Malori <abo7fg@virginia.edu>
2025-05-13 15:06:12 -0400

    First commit

---
+++ asv_arov_localization/arov_ekf_global.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from asv_arov_localization.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'asv_arov_localization'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='maddy',
+    maintainer_email='abo7fg@virginia.edu',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'arov_ekf_global = asv_arov_localization.arov_ekf_global:main'
+        ],
+    },
+)
